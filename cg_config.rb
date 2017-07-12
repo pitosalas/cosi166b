1ee00ff99988c6ca3e760473bdf2870296d09f2d
Pito Salas <pitosalas@gmail.com>
2017-07-12 11:09:48 -0400

    first commit

---
+++ cg_config.rb
@@ -0,0 +1,43 @@
+require 'coursegen'
+
+# bucket for AWS Deployment of the course
+AWS_BUCKET = "cosi166b"
+
+# Local directory path for directory of content
+CONTENT_PATH = "/mydev/cosi166b"
+
+# Copyright string
+COPYRIGHT_STRING = "Copyright (2014-2017) R. Pito Salas, pitosalas@brandeis.edu"
+
+# Course short name
+COURSE_SHORT_NAME = "COSI 166b"
+COURSE_LONG_NAME = "Software Engineering"
+COURSE_ABBREV = "166b"
+
+# Schedule information. Note that Monday is day 0
+LECTURES_SCHEDULE_CONFIG = ScheduleDef.new(
+  first_day: "aug-31-2017",
+  weekdays: [:tuesday, :thursday, :friday],
+  start_times: ["15:30", "15:30", "14:00"],
+  end_times: ["16:50", "16:50", "16:00"],
+  start_time: "15:30",
+  end_time: "16:50",
+  number: 39,
+  skips: [])
+
+# Sections in the right hand margin of the page
+# Bullet choices are :dash, :star, :plus, :minus
+SECTION_CONFIG = [
+  SectionDef.new("Introduction", "intro", hidden: false, type: :section, bullet: :star),
+  SectionDef.new("Lectures", "lectures", type: :lecture, schedule: LECTURES_SCHEDULE_CONFIG, bullet: :dash),
+  SectionDef.new("Term Project", "project", hidden: false, type: :section),
+  SectionDef.new("Background", "background", hidden: false, type: :section),
+  SectionDef.new("Topics", "topics", hidden: true, type: :section),
+  SectionDef.new("Extras", "extras", hidden: true, type: :section)
+]
+
+# Options are:
+# bullet_style: css style for each bullet
+STYLING_CONFIG = {bullet_style: "\"font-size: 60%; width: 10px; color: grey\""}
+HOME_PAGE = "/content/intro/00_syllabus.md.erb"
+HELPFUL_BOX = false
---
+++ retrospective.md
@@ -0,0 +1,14 @@
+## Notes from experience teaching in 2017
+
+* add a significant new amount of javascript
+* space out the PARails 1,2,3 so that they match better with the material
+* look closely at balancing of homeworks so that they don't clump together
+* look closely at adding justifications to homework assignments
+* add a little more and structured stuff about agile
+* make sure the cocktail hour is there
+* Maybe ask Jen Cleary to do some kind of teaming workshop during one of the early classes
+
+
+* Here are two examples of lectures with too many and too confusing assignments:
+http://cosi166b-s2017.s3-website-us-west-2.amazonaws.com/content/lectures/iter2/07_sprint_1.md/ and
+http://cosi166b-s2017.s3-website-us-west-2.amazonaws.com/content/lectures/iter3/08_web_services_cont.md/
